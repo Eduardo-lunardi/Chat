7edd2d17ea0740ce10565801471bb2983d7201cc
Eduardo <eduardolunardi942@gmail.com>
2020-04-26 01:36:26 -0300

    a

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/src/Index.html');
 });
 
 io.on('connection', function(socket){
@@ -14,5 +14,28 @@ io.on('connection', function(socket){
 });
 
 http.listen(port, function(){
-  console.log('listening on *:' + port);
+  console.log('conectado na porta:' + port);
+});
+
+io.on('connection', function(socket){
+  socket.join('some room');
+});
+
+
+// Iniciando Socket.IO
+var visitas = 0;
+// Evento connection ocorre quando entra um novo usuário.
+io.on('connection', function(socket){
+  // Incrementa o total de visitas no site.
+  visitas++;
+  // Envia o total de visitas para o novo usuário.
+  socket.emit('visits', visitas);
+  // Envia o total de visitas para os demais usuários.
+  socket.broadcast.emit('visits', visitas);
+  // Evento disconnect ocorre quando sai um usuário.
+  socket.on('disconnect', function(){
+    visitas--;
+    // Atualiza o total de visitas para os demais usuários.
+    socket.broadcast.emit('message', visitas);
+  });
 });
